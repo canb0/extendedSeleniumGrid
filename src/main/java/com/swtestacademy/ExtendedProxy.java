49d1c1758502d936442703b7ec2eb483895c703b
canboSaha <Gulin-2013>
2018-05-14 11:18:05 +0300

    Video Recording Capability Added

--- src/main/java/com/swtestacademy/ExtendedProxy.java
+++ src/main/java/com/swtestacademy/ExtendedProxy.java
@@ -15,16 +15,14 @@ public class ExtendedProxy extends DefaultRemoteProxy  implements TestSessionLis
     public ExtendedProxy(RegistrationRequest request, GridRegistry registry) {
         super(request, registry);
     }
-
+    private static Recorder screenRecorder;
+    private static String RECORD_VIDEO = "recordVideo";
+    private static Boolean record;
 
     @Override
     public void beforeCommand(TestSession session, HttpServletRequest request,
                               HttpServletResponse response) {
         System.out.println("****** SWTESTACADEMY *****");
-        System.out.println("Selenium Extending Grid - Before Command");
-        System.out.println("Method " +request.getMethod());
-        System.out.println("getRequestURI " +request.getRequestURI());
-        System.out.println("Session "+session.toString());
 
 
     }
@@ -33,24 +31,23 @@ public class ExtendedProxy extends DefaultRemoteProxy  implements TestSessionLis
     @Override
     public void afterCommand(TestSession session, HttpServletRequest request,
                               HttpServletResponse response) {
-        System.out.println("****** SWTESTACADEMY *****");
-        System.out.println("Selenium Extending Grid - After Command");
     }
 
     @Override
     public void beforeSession(TestSession session){
-        System.out.println("****** SWTESTACADEMY *****");
-        System.out.println("Selenium Extending Grid - Before Session");
-        System.out.println("Internal key "+session.getInternalKey());
-
-
+        record = (Boolean)session.getRequestedCapabilities().get(RECORD_VIDEO);
+        if (record) {
+            screenRecorder = new Recorder();
+            screenRecorder.startScreenRecorder();
+            System.out.println("Video Recording value is "+record);
+        }
     }
 
     @Override
     public void afterSession(TestSession session){
-        System.out.println("****** SWTESTACADEMY *****");
         System.out.println("Selenium Extending Grid - After Session");
-
+        if(record)
+            screenRecorder.stopScreenRecorder(session.getInternalKey());
 
     }
 
